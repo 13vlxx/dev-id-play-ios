8fd81763bd3afa9d80333209221b98fcaed0229f
alex <arusescu@dev-id.fr>
2024-07-15 11:27:08 +0200

    Initial Commit

---
+++ dev-id-play-ios/dev_id_play_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_id_play_iosApp.swift
+//  dev-id-play-ios
+//
+//  Created by Alex ツ on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct dev_id_play_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
